7e62a8b3f8f106d01c6f41b81d0a402f3360dcc0
PavithraMegaraj <pavithramegaraj@gmail.com>
2026-09-15 11:02:17 +0530

    First Commit for setup

---
+++ src/main/java/com/first/controller/firstcontroller.java
@@ -0,0 +1,17 @@
+package com.first.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first")
+public class firstcontroller {
+
+    @GetMapping
+    public String getStatus()
+    {
+        return "Application is running";
+    }
+
+}
